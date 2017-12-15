03346b495203f1b8f707aba130b0d890e151a2de
Lelouch-Xue <beimengtianxia@gmail.com>
2017-12-15 10:19:04 +0800

    Update

---
+++ Fonts/Demo/Editor/ScreenShotsEditor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+public class ScreenShotsEditor {
+
+    [MenuItem("Lelouch/ScreenShots")]
+    public static void ScreenShots()
+    {
+        GameObject go = Selection.activeGameObject;
+
+        if (!go)
+        {
+            Debug.LogErrorFormat("Please sure you selected one gameobject!");
+        }
+        Text text = go.transform.Find("Title").GetComponent<Text>();
+        if (text)
+        {
+            string n = text.font.name;
+            Debug.LogFormat("Current text font is : {0}",n);
+            text.text = text.font.name;
+
+            ScreenCapture.CaptureScreenshot(Application.dataPath + "/" + n +".png", 1);
+            AssetDatabase.Refresh();
+            
+        }
+    }
+}
